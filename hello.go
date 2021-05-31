7cb6e4a4c77af07fdff21cbd90a64fbd51d92bd3
jonatollah <jonatollah@gmail.com>
2021-05-31 17:32:36 -0500

    golang

--- hello.go
+++ hello.go
@@ -5,3 +5,4 @@ import "rsc.io/quote"
 func Hello() string {
     return quote.Hello()
 }
+
--- short_declaration.go
+++ short_declaration.go
@@ -1,6 +1,28 @@
 package main
 import("fmt")
+var z = 22
+var y int =0
 func main(){
 	x :=42
 	fmt.Println(x)
+	x = 99
+	fmt.Println(x)
+	y := 100 +24 +y
+	foo()
+	fmt.Println(y)
+	d:="||||||"
+	fmt.Println(d)
+	fmt.Printf("%T\n",y)
+
+	fmt.Printf("%b\n",y)
+	fmt.Printf("%x\n",y)
+	y=911
+	fmt.Printf("%#x\n",y)
+	s:= fmt.Sprintf("%#x\t%b\t%x\n",y,y,y)
+	fmt.Println(s)
+}
+
+func foo(){
+fmt.Println("again",z)
+
 }
